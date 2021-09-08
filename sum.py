a518714c0d08b481170ca304a60d7c6866ab94dc
Sophie <sbell52@student.gsu.edu>
2021-09-07 23:51:14 -0400

    Added Comment at the top of the Python Code for the sum of 1-100

--- sum.py
+++ sum.py
@@ -1,6 +1,6 @@
 Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> sum = 0
+>>> sum = 0                  # This function will take a range from 1-100 and print the sum of that range
 >>> for i in range(1,101):
 	sum +=i
 
